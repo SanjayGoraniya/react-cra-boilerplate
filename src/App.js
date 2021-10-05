546af6dec09187efb4f28eded0d2f42c09f54e54
Sanjay Goraniya <sgoraniya22@gmail.com>
2021-10-05 15:47:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
+          Learn React
+        </a>
+      </header>
+      <footer class="relative pt-8 pb-6 mt-16">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-wrap items-center md:justify-between justify-center">
+                <div class="w-full md:w-6/12 px-4 mx-auto text-center">
+                    <div class="text-sm text-blueGray-500 font-semibold py-1">
+                        Made with Love by <a href="https://sanjaygoraniya.github.io" class="hover:text-blueGray-500 text-blueGray-800" target="_blank" rel="noopener noreferrer">Sanjay Goraniya</a>.
+                    </div>
+                </div>
+            </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
