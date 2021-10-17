f6cecd71c0f981015087e3c8464f59391895b76a
Sanjay Goraniya <52008817+SanjayGoraniya@users.noreply.github.com>
2021-10-17 23:05:48 +0530

    Fixed : React warnings

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ function App() {
           Learn React
         </a>
       </header>
-      <footer class="relative pt-8 pb-6 mt-16">
-        <div class="container mx-auto px-4">
-            <div class="flex flex-wrap items-center md:justify-between justify-center">
-                <div class="w-full md:w-6/12 px-4 mx-auto text-center">
-                    <div class="text-sm text-blueGray-500 font-semibold py-1">
-                        Made with Love by <a href="https://sanjaygoraniya.github.io" class="hover:text-blueGray-500 text-blueGray-800" target="_blank" rel="noopener noreferrer">Sanjay Goraniya</a>.
+      <footer className="relative pt-8 pb-6 mt-16">
+        <div className="container mx-auto px-4">
+            <div className="flex flex-wrap items-center md:justify-between justify-center">
+                <div className="w-full md:w-6/12 px-4 mx-auto text-center">
+                    <div className="text-sm text-blueGray-500 font-semibold py-1">
+                        Made with Love by <a href="https://sanjaygoraniya.github.io" className="hover:text-blueGray-500 text-blueGray-800" target="_blank" rel="noopener noreferrer">Sanjay Goraniya</a>.
                     </div>
                 </div>
             </div>
